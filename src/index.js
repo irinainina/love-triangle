411b7a2df3f38e847cb099c8de0c72d5067b1771
Ирина <irbris@mail.ru>
2019-02-19 15:17:09 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+module.exports = function getLoveTrianglesCount(arr) {
+  var count = 0;
+  for(let i = 0; i < arr.length; i++) {
+    var one = arr[i]; 
+    var two = arr[one - 1]; 
+    var three = arr[two - 1]; 
+    if(one === i + 1) continue;
+    if(three === i + 1) count++;
+  }
+  return count / 3;
 };
